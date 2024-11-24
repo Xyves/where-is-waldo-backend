704fe7b3e44e36f57d25caf3141528e85a37902a
Xyves <xodrixx@protonmail.com>
2024-11-24 11:32:32 +0100

    Implement DB query logic for Fetching and Posting Scores in Controllers

---
+++ controllers/scoreboardController.ts
@@ -0,0 +1,24 @@
+export const fetchScores = async (req: any, res: any) => {
+  const results = await prisma.scoreboard.findMany();
+  return res.status(200).json({ results });
+};
+export const postScore = async (req: any, res: any) => {
+  const { name, time } = req.body;
+  if (!name || !time) {
+    return res.status(400).json({ error: "name and time are required" });
+  }
+
+  try {
+    const score = await prisma.scoreboard.create({
+      data: { name: name, time: time },
+    });
+
+    return res.status(201).json(score);
+  } catch (error) {
+    console.error("Error creating comment:", error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while creating the new score" });
+  }
+};
+module.exports = { fetchScores, postScore };
