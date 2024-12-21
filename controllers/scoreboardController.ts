51ba147fb28fe990eab4dce7390da4fa1328b845
Xyves <xodrixx@protonmail.com>
2024-12-21 18:00:28 +0100

    Add sorting scores by time

--- controllers/scoreboardController.ts
+++ controllers/scoreboardController.ts
@@ -1,7 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 export const fetchScores = async (req: any, res: any) => {
-  const results = await prisma.scoreboard.findMany();
+  const results = await prisma.scoreboard.findMany({
+    orderBy: {
+      time: "asc",
+    },
+  });
   return res.status(200).json({ results });
 };
 export const postScore = async (req: any, res: any) => {
